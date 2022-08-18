eb27c55bdf086b5c0261a14c04938a287abe874e
Makariuz <samuel.eduardo23@hotmail.com>
2022-08-18 13:01:28 +0100

    meat prices scrap continente

--- app.js
+++ app.js
@@ -14,33 +14,58 @@ app.use(cors())
 const PORT = process.env.port || 5001;
 
 const website = process.env.website;
-
+let content = [];
 
 try {
     axios(website).then((res) => {
       const data = res.data;
       const $ = cheerio.load(data);
 
-      let content = [];
-  
+      
+        
       $(process.env.class, data).each(function () {
         const title = $(this).text();
-        const url = $(this).find('a').attr('href');
+        const price = $('.ct-price-formatted').html()
   
         content.push({
           title,
-          url,
+          price
         });
   
         app.get('/', (req, res) => {
           res.json(content);
-          console.log(content)
+
         });
       });
     });
   } catch (error) {
     console.log(error, error.message);
   }
+   /* try {
+    axios(website).then((res) => {
+      const data = res.data;
+      const $ = cheerio.load(data);
+
+        
+      $('.ct-price-formatted', data).each(function () {
+        const price = $(this).text();
+
+        const url = $(this).find('a').attr('href');
+  
+        content.push({
+          price,
+          
+        });
+  
+        app.get('/', (req, res) => {
+          res.json(content);
+
+        });
+      });
+    });
+  } catch (error) {
+    console.log(error, error.message);
+  }  */
 
 app.listen(PORT, () => {
   console.log(`server is running on PORT:${PORT}`);
